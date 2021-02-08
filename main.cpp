f78569258f60ccd476a66eeaa4d6caae55d8a06e
vsimoes29 <vincent.simoes@imascap.com>
2021-02-08 10:32:39 +0100

    Correction Ex1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
-
+#include <itkImage.h>
 
 /*---- QT Includes ----*/
 #include <qdebug.h>
@@ -16,31 +16,42 @@ int main(int p_argc, char* p_argv[])
 	typedef itk::Image<unsigned char, 2> UCharImageType;
 
 	//Create image
-
-
-	//Define starting point (0, 0)
-        UCharImageType::IndexType startIndex;
+	UCharImageType::Pointer image = UCharImageType::New();
 
 	//Define image size
-        UCharImageType::SizeType size;
+	UCharImageType::SizeType size;
+	size[0] = 512;
+	size[1] = 512;
 
-        //Define image region
+	//Define image region
+	UCharImageType::RegionType region;
+	region.SetSize(size);
 
 	//Set the region to the image
 	image->SetRegions(region);
 
 	//Set image origin (where it starts in a 2D/3D referentiel)
 	UCharImageType::PointType origin;
-
+	origin[0] = 0;
+	origin[1] = 0;
+	image->SetOrigin(origin);
 
 	//Set image spacing
 	UCharImageType::SpacingType spacing;
-
+	spacing[0] = 0.5;
+	spacing[1] = 0.5;
+	image->SetSpacing(spacing);
 
 	//Allocate the image in memory
+	image->Allocate();
 
 	//Check image
-        qDebug() << "Dimension X:" << image->GetLargestPossibleRegion().GetSize()[0];
-        //....
+	qDebug() << "Dimension X:" << image->GetLargestPossibleRegion().GetSize()[0];
+	qDebug() << "Dimension Y:" << image->GetLargestPossibleRegion().GetSize()[1];
+	qDebug() << "Spacing X:" << image->GetSpacing()[0];
+	qDebug() << "Spacing Y:" << image->GetSpacing()[1];
+	qDebug() << "Origin X:" << image->GetOrigin()[0];
+	qDebug() << "Origin Y:" << image->GetOrigin()[1];
+
 	return 0;
 } 
