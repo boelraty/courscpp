37d84a79566e7e2ae7765ce1c2bcee8acf9cec57
vsimoes29 <vincent.simoes@imascap.com>
2021-02-08 11:04:59 +0100

    Add ITK Ex4

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple3
+/*!	\brief	Exemple4
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
 #include <itkImage.h>
 #include <itkImageRegionConstIterator.h>
-#include <itkImageRegionIterator.h>
-#include <itkImageFileWriter.h>
+#include <itkImageFileReader.h>
 
 /*---- QT Includes ----*/
 #include <qdebug.h>
@@ -18,133 +17,36 @@ int main(int p_argc, char* p_argv[])
 	//Typedef to define a image type
 	typedef itk::Image<unsigned char, 2> UCharImageType;
 
-	//Create image
-	UCharImageType::Pointer image = UCharImageType::New();
+	//Create the reader to read an image
+	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
+	reader->SetFileName("/home/vsimoes/image.png");
+	reader->Update();
 
-	//Define starting point (0, 0)
-	UCharImageType::IndexType startIndex;
-	startIndex[0] = 0;
-	startIndex[1] = 0;
+	//Write dimensions and spacing
+	qDebug() << "Dimensions X :" << reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
+	qDebug() << "Dimensions Y :" << reader->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
+	qDebug() << "Spacing X :" << reader->GetOutput()->GetSpacing()[0];
+	qDebug() << "Spacing Y :" << reader->GetOutput()->GetSpacing()[1];
 
-	//Define image size
-	UCharImageType::SizeType size;
-	size[0] = 512;
-	size[1] = 512;
+	//Create an iterator to check if the image contains the rectangle
+	itk::ImageRegionConstIterator<UCharImageType> iterator(reader->GetOutput(), reader->GetOutput()->GetLargestPossibleRegion());
+	
+	//Initialize iterator to begin (GoToBegin())
+	iterator.GoToBegin();
 
-	//Define image region
-	UCharImageType::RegionType region;
-	region.SetIndex(startIndex);
-	region.SetSize(size);
-
-	//Set the region to the image
-	image->SetRegions(region);
-
-	//Set image origin (where it starts in a 2D/3D referentiel)
-	UCharImageType::PointType origin;
-	origin[0] = 0;
-	origin[1] = 0;
-	image->SetOrigin(origin);
-
-	//Set image spacing
-	UCharImageType::SpacingType spacing;
-	spacing[0] = 0.5;
-	spacing[1] = 0.5;
-	image->SetSpacing(spacing);
-
-	//Allocate the image in memory
-	image->Allocate();
-	image->FillBuffer(0);
-
-
-	//START EXERCICE 2
-
-	//Create const iterator for the iteration on the entire image
-	itk::ImageRegionConstIterator<UCharImageType> globalIterator(image, image->GetLargestPossibleRegion());
-
-	// Initialize iterator to beginning
-	globalIterator.GoToBegin();
-
-	//Iterate over the entire image to count the number of pixels
 	int nbPixels = 0;
 
-	while (! globalIterator.IsAtEnd())
+	while(!iterator.IsAtEnd())
 	{
-		//To print the pixel value
-		//qDebug() << globalIterator.Get();
-
-		//Increment number of pixels
-		++nbPixels;
+		//Check if pixel = 255
+		if(iterator.Get() == static_cast<unsigned char>(255))
+			++nbPixels;
 
 		//Increment iterator
-		++globalIterator;
+		++iterator;
 	}
 
-	qDebug() << "Number of pixels (512, 512) =" << nbPixels;
-
-	//Create a region for iterate over a part of the image
-	UCharImageType::RegionType regionForIteration;
-
-	//Define starting pixel for iteration
-	UCharImageType::IndexType startingPixel;
-	startingPixel[0] = 50;
-	startingPixel[1] = 100;
-
-	//Define size of region for iteration
-	UCharImageType::SizeType regionSize;
-	regionSize[0] = 100;
-	regionSize[1] = 200;
-
-	regionForIteration.SetIndex(startingPixel);
-	regionForIteration.SetSize(regionSize);
-
-	//Create non const iterator for fill the image with the square
-	itk::ImageRegionIterator<UCharImageType> localIterator(image, regionForIteration);
-	localIterator.GoToBegin();
-
-	nbPixels = 0;
-
-	while(!localIterator.IsAtEnd())
-	{
-		//Set a value to the pixel
-		localIterator.Set(static_cast<unsigned char>(255));
-
-		//Increment number of set pixels
-		++nbPixels;
-
-		//Increment iterator
-		++localIterator;
-	}
-
-	qDebug() << "Number of pixels set:" << nbPixels;
-
-	//Access to a specific pixel
-	UCharImageType::IndexType pixelIndex1;
-	pixelIndex1[0] = 60;
-	pixelIndex1[1] = 150;
-
-	UCharImageType::IndexType pixelIndex2;
-	pixelIndex2[0] = 200;
-	pixelIndex2[1] = 150;
-
-	qDebug() << "Pixel(60, 150) =" << image->GetPixel(pixelIndex1);
-	qDebug() << "Pixel(200, 150) =" << image->GetPixel(pixelIndex2);
-
-	//In order to catch possible error
-	try
-	{
-		//Create the filter to write the image
-		itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-        writer->SetFileName("toto.png");
-		writer->SetInput(image);
-		writer->Write();
-		//writer->Update();
-		qDebug() << "Writing done";
-	}
-	catch(itk::ExceptionObject & ex)
-	{
-		std::cout << ex.what();
-	}
-
+	qDebug() << "Nb pixels equal to 255 :" << nbPixels;
 
 	return 0;
 } 
