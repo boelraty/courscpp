e37a751c92f7eef71641012a247d7102850e234a
vsimoes29 <vincent.simoes@imascap.com>
2021-02-08 11:13:11 +0100

    Add ITK Ex5

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple4
+/*!	\brief	Exemple5
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
 #include <itkImage.h>
-#include <itkImageRegionConstIterator.h>
 #include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+#include <itkGDCMImageIO.h>
 
 /*---- QT Includes ----*/
 #include <qdebug.h>
 
+/*---- STL Includes ----*/
+#include <string>
+
 
 int main(int p_argc, char* p_argv[])
 {
-	//Typedef to define a image type
-	typedef itk::Image<unsigned char, 2> UCharImageType;
+	//Typedef to define a image type (refer to exercice 1 and replace unsigned char with signed short)
+	typedef itk::Image<signed short, 2> ShortImageType;
+
+	//Create the GDCM image IO
+	itk::GDCMImageIO::Pointer gdcmImageIO = itk::GDCMImageIO::New();
 
 	//Create the reader to read an image
-	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
-	reader->SetFileName("/home/vsimoes/image.png");
+	itk::ImageFileReader<ShortImageType>::Pointer reader = itk::ImageFileReader<ShortImageType>::New();
+	reader->SetFileName("/home/vsimoes/Documents/Formations/Donnees/CoursISEN/Data/ImageDICOM");
+	reader->SetImageIO(gdcmImageIO);
 	reader->Update();
 
 	//Write dimensions and spacing
@@ -28,25 +36,35 @@ int main(int p_argc, char* p_argv[])
 	qDebug() << "Spacing X :" << reader->GetOutput()->GetSpacing()[0];
 	qDebug() << "Spacing Y :" << reader->GetOutput()->GetSpacing()[1];
 
-	//Create an iterator to check if the image contains the rectangle
-	itk::ImageRegionConstIterator<UCharImageType> iterator(reader->GetOutput(), reader->GetOutput()->GetLargestPossibleRegion());
-	
-	//Initialize iterator to begin (GoToBegin())
-	iterator.GoToBegin();
+	//Check information from the GDCM ImageIO
+	char * patientname = new char;
+	char * patientbirthdate = new char;
+	char * patientid = new char;
+	char * studydate = new char;
+	std::string modality;
 
-	int nbPixels = 0;
+	//Get DICOM data
+	gdcmImageIO->GetPatientName(patientname);
+	gdcmImageIO->GetPatientDOB(patientbirthdate);
+	gdcmImageIO->GetPatientID(patientid);
+	gdcmImageIO->GetStudyDate(studydate);
+	gdcmImageIO->GetValueFromTag(std::string("0008|0060"), modality);
 
-	while(!iterator.IsAtEnd())
-	{
-		//Check if pixel = 255
-		if(iterator.Get() == static_cast<unsigned char>(255))
-			++nbPixels;
+	qDebug() << "Patient name :" << patientname;
+	qDebug() << "Patient birthdate :" << patientbirthdate;
+	qDebug() << "Patient id :" << patientid;
+	qDebug() << "Study date :" << studydate;
+	qDebug() << "Modality :" << modality.c_str();
 
-		//Increment iterator
-		++iterator;
-	}
+    itk::ImageFileWriter<ShortImageType>::Pointer writer = itk::ImageFileWriter<ShortImageType>::New();
+    writer->SetInput(reader->GetOutput());
+    writer->SetFileName("image.tiff");
+    writer->Write();
 
-	qDebug() << "Nb pixels equal to 255 :" << nbPixels;
-
+	//Delete allocated pointers
+	delete [] patientname;
+	delete [] patientbirthdate;
+	delete [] patientid;
+	delete [] studydate;
 	return 0;
 } 
