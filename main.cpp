407dcf21ca578db4f75831623d5af77c10ca5806
vsimoes29 <vincent.simoes@imascap.com>
2021-02-12 08:30:34 +0100

    Correction Ex6

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
-#include <itkBinaryThresholdImageFilter.h>
 #include <itkImage.h>
 #include <itkImageFileReader.h>
+#include <itkBinaryThresholdImageFilter.h>
 #include <itkImageFileWriter.h>
-#include <itkGDCMImageIO.h>
 
 /*---- QT Includes ----*/
 #include <qdebug.h>
@@ -25,28 +24,26 @@ int main(int p_argc, char* p_argv[])
 		typedef itk::Image<signed short, 2> ShortImageType;
 		typedef itk::Image<unsigned char, 2> UCharImageType;
 
-
-		//Create the GDCM image IO
-		itk::GDCMImageIO::Pointer gdcmImageIO = itk::GDCMImageIO::New();
-
 		//Create the reader to read an image
 		itk::ImageFileReader<ShortImageType>::Pointer reader = itk::ImageFileReader<ShortImageType>::New();
-		reader->SetFileName("D:/Owncloud/CoursISEN/Data");
-		reader->SetImageIO(gdcmImageIO);
+		reader->SetFileName("/home/vsimoes/Documents/Formations/Donnees/CoursISEN/Data/ImageDICOM");
 		reader->Update();
 
 		//Create filter to apply threshold on image
 		itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::Pointer thresholder =
-			itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::New();
-		//Use reader->GetOutput() as input of thresholder
-                        
-
+				itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::New();
+		thresholder->SetInput(reader->GetOutput());
+		thresholder->SetLowerThreshold(160);
+		//thresholder->SetUpperThreshold(1000);
+		thresholder->SetInsideValue(255);
+		thresholder->SetOutsideValue(0);
+		thresholder->Update();
 
 		//Write the binary image
-        itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-		//Refer to ex 3 and use output of thresholder
-		
-		
+		itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
+		writer->SetFileName("/home/vsimoes/binaryimage.png");
+		writer->SetInput(thresholder->GetOutput());
+		writer->Update();
 	}
 	catch(itk::ExceptionObject & ex)
 	{
