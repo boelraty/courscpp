002e9f714f7be89277700670987d0639d08d3480
vsimoes29 <vincent.simoes@imascap.com>
2021-02-12 08:35:29 +0100

    Init Ex7

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple6
+/*!	\brief	Exemple7
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
 #include <itkImage.h>
 #include <itkImageFileReader.h>
-#include <itkBinaryThresholdImageFilter.h>
 #include <itkImageFileWriter.h>
+#include <itkConnectedComponentImageFilter.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkImageFileWriter.h>
+#include <itkRescaleIntensityImageFilter.h>
 
 /*---- QT Includes ----*/
-#include <qdebug.h>
+#include <QDebug>
 
 /*---- STL Includes ----*/
 #include <string>
@@ -18,36 +21,40 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	try
-	{
-		//Typedef to define a image type
-		typedef itk::Image<signed short, 2> ShortImageType;
-		typedef itk::Image<unsigned char, 2> UCharImageType;
+	//Typedef to define a image type
+	typedef itk::Image<unsigned char, 2> UCharImageType;
 
-		//Create the reader to read an image
-		itk::ImageFileReader<ShortImageType>::Pointer reader = itk::ImageFileReader<ShortImageType>::New();
-		reader->SetFileName("/home/vsimoes/Documents/Formations/Donnees/CoursISEN/Data/ImageDICOM");
-		reader->Update();
+	typedef itk::Image<int, 2> IntImageType;
 
-		//Create filter to apply threshold on image
-		itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::Pointer thresholder =
-				itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::New();
-		thresholder->SetInput(reader->GetOutput());
-		thresholder->SetLowerThreshold(160);
-		//thresholder->SetUpperThreshold(1000);
-		thresholder->SetInsideValue(255);
-		thresholder->SetOutsideValue(0);
-		thresholder->Update();
+	//Create the reader to read an image
+	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
+	reader->SetFileName("/home/vsimoes/binaryimage.png");
+	reader->Update();
+	
+	
+	//Labelize the image
+	itk::ConnectedComponentImageFilter<UCharImageType, IntImageType>::Pointer labeler =
+			itk::ConnectedComponentImageFilter<UCharImageType, IntImageType>::New();
+	//Set input = outout of reader
 
-		//Write the binary image
-		itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-		writer->SetFileName("/home/vsimoes/binaryimage.png");
-		writer->SetInput(thresholder->GetOutput());
-		writer->Update();
-	}
-	catch(itk::ExceptionObject & ex)
-	{
-		std::cout << ex.what();
-	}
+	
+	//Relabelize the image to get info about the group of found pixels
+//	itk::RelabelComponentImageFilter<IntImageType, UCharImageType>::Pointer relabeler =
+//		itk::RelabelComponentImageFilter<IntImageType, UCharImageType>::New();
+
+
+//	qDebug() << "Number of found objects :" << relabeler->GetNumberOfObjects();
+//	for (int i = 0 ; i < relabeler->GetNumberOfObjects() ; ++i)
+//		qDebug() << "Area of objects " << i << ":" << relabeler->GetSizeOfObjectInPhysicalUnits(i);
+
+	//Rescale the image to see colors on labelized output
+//    itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType>::Pointer rescaler =
+//    ...
+
+	//Write the binary image
+//	itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType>::Pointer rescaler =
+//		itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType>::New();
+//  //...
+
 	return 0;
 } 
