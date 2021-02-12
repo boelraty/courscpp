1b5564ea0728bc38456bf8dedeeaf31777381fa2
vsimoes29 <vincent.simoes@imascap.com>
2021-02-12 10:16:55 +0100

    Init VTK Ex1

--- main.cpp
+++ main.cpp
@@ -1,77 +1,25 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple8
+/*!	\brief	Exemple1
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
-/*---- ITK Includes ----*/
-#include <itkImage.h>
-#include <itkImageFileReader.h>
-#include <itkBinaryBallStructuringElement.h>
-#include <itkBinaryDilateImageFilter.h>
-#include <itkBinaryErodeImageFilter.h>
-#include <itkGrayscaleFillholeImageFilter.h>
-#include <itkImageFileWriter.h>
+/*---- VTK Includes ----*/
+#include <vtkSmartPointer.h>
 
 /*---- QT Includes ----*/
-#include <QDebug>
-
-/*---- STL Includes ----*/
-#include <string>
+#include <qdebug.h>
 
 
 int main(int p_argc, char* p_argv[])
 {
-	//Typedef to define a image type
-	typedef itk::Image<unsigned char, 2> UCharImageType;
-
-	//Create the reader to read an image
-	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
-	reader->SetFileName("D:/image_seuillee.png");
-	reader->Update();
-
-	//Typedef to define a StructuringElement type
-	typedef itk::BinaryBallStructuringElement <unsigned char, 2> StructuringElementType;
-
-	StructuringElementType structuringElement;
-	structuringElement.SetRadius( 1 );
-	structuringElement.CreateStructuringElement();
-
-	//Typedef to define a DilateFilter type
-	typedef itk::BinaryDilateImageFilter <	UCharImageType,
-											UCharImageType,
-											StructuringElementType> DilateFilterType;
-
-	//Typedef to define a ErodeFilter type
-	typedef itk::BinaryErodeImageFilter <	UCharImageType,
-											UCharImageType,
-											StructuringElementType> ErodeFilterType;
-
-	ErodeFilterType::Pointer eroder = ErodeFilterType::New();
-	eroder->SetKernel(structuringElement);
-	eroder->SetInput(reader->GetOutput());
-	eroder->SetErodeValue( 255 );
-	eroder->Update();
-
-	DilateFilterType::Pointer dilater = DilateFilterType::New();
-	dilater->SetKernel( structuringElement );
-	dilater->SetInput( eroder->GetOutput() );
-	dilater->SetDilateValue( 255 );
-	dilater->Update();
-
-	//Typedef to define a FillFilter Type
-	typedef itk::GrayscaleFillholeImageFilter < UCharImageType, UCharImageType > FillFilterType;
-
-	FillFilterType::Pointer filler = FillFilterType::New();
-	filler->SetInput(dilater->GetOutput());
-	filler->Update();
+	// Create a sphere
+	vtkSmartPointer<vtkSphereSource> sphereObject = vtkSmartPointer<vtkSphereSource>::New();
 
 
-	//Write the binary image
-	itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-	writer->SetFileName("D:/resultImage3.png");
-	writer->SetInput(filler->GetOutput());
-	writer->Update();
+	// Get bounds of resulting polydata
 
+
+    //delete [] bounds;
 
 	return 0;
 } 
