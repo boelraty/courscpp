f8d0e875c782ac9b028b121a25ad27352fd1c778
vsimoes29 <vincent.simoes@imascap.com>
2021-02-12 09:12:20 +0100

    Init ITK Ex8

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple7
+/*!	\brief	Exemple8
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
 #include <itkImage.h>
 #include <itkImageFileReader.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkBinaryDilateImageFilter.h>
+#include <itkBinaryErodeImageFilter.h>
+#include <itkGrayscaleFillholeImageFilter.h>
 #include <itkImageFileWriter.h>
-#include <itkConnectedComponentImageFilter.h>
-#include <itkRelabelComponentImageFilter.h>
-#include <itkImageFileWriter.h>
-#include <itkRescaleIntensityImageFilter.h>
 
 /*---- QT Includes ----*/
 #include <QDebug>
@@ -24,47 +24,39 @@ int main(int p_argc, char* p_argv[])
 	//Typedef to define a image type
 	typedef itk::Image<unsigned char, 2> UCharImageType;
 
-	typedef itk::Image<int, 2> IntImageType;
-
 	//Create the reader to read an image
 	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
-	reader->SetFileName("D:/image_seuillee.png");
+	reader->SetFileName("/home/vsimoes/binaryimage.png");
 	reader->Update();
 
-	//Labelize the image
-	itk::ConnectedComponentImageFilter<UCharImageType, IntImageType>::Pointer labeler =
-			itk::ConnectedComponentImageFilter<UCharImageType, IntImageType>::New();
-	labeler->SetInput(reader->GetOutput());
-	labeler->Update();
+	//Typedef to define a StructuringElement type
+	typedef itk::BinaryBallStructuringElement <unsigned char, 2> StructuringElementType;
 
-	//Relabelize the image to get info about the group of found pixels
-	itk::RelabelComponentImageFilter<IntImageType, UCharImageType>::Pointer relabeler =
-			itk::RelabelComponentImageFilter<IntImageType, UCharImageType>::New();
-	relabeler->SetInput(labeler->GetOutput());
-	relabeler->SetMinimumObjectSize(100);
-	relabeler->Update();
+    //Create structuring element
+	StructuringElementType structuringElement;
+	structuringElement.SetRadius( 1 );
+	structuringElement.CreateStructuringElement();
 
-    itk::ImageFileWriter<UCharImageType>::Pointer writer2 = itk::ImageFileWriter<UCharImageType>::New();
-    writer2->SetInput(relabeler->GetOutput());
-    writer2->SetFileName("D:/output.png");
-    writer2->Update();
+	//Typedef to define a DilateFilter type
+	typedef itk::BinaryDilateImageFilter <	UCharImageType, UCharImageType, StructuringElementType> DilateFilterType;
+
+	//Typedef to define a ErodeFilter type
+	typedef itk::BinaryErodeImageFilter <	UCharImageType, UCharImageType, StructuringElementType> ErodeFilterType;
+
+	//Erode image
+	ErodeFilterType::Pointer eroder = ErodeFilterType::New();
+
+	//Dilate image
+	DilateFilterType::Pointer eroder = DilateFilterType::New();
 
 
-	qDebug() << "Number of found objects :" << relabeler->GetNumberOfObjects();
-	for (int i = 0 ; i < relabeler->GetNumberOfObjects() ; ++i)
-		qDebug() << "Area of objects " << i << ":" << relabeler->GetSizeOfObjectInPhysicalUnits(i);
+	//Typedef to define a FillFilter Type
+	typedef itk::GrayscaleFillholeImageFilter < UCharImageType, UCharImageType > FillFilterType;
 
-	//Rescale the image to see colors on labelized output
-	itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType>::Pointer rescaler =
-			itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType>::New();
-	rescaler->SetInput(relabeler->GetOutput());
-	rescaler->Update();
+	//Fill holes in image
+
 
 	//Write the binary image
-	itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-	writer->SetFileName("D:/labelizedimage.png");
-	writer->SetInput(rescaler->GetOutput());
-	writer->Update();
 
 	return 0;
 } 
