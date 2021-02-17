79b774a24e8d1adaee87cca62ab3f20d3f9e0100
vsimoes29 <vincent.simoes@imascap.com>
2021-02-17 17:36:01 +0100

    Qt Ex4

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple3
+/*!	\brief	Exemple4
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
+
+/*---- Internal Includes ----*/
+#include "Widget.h"
 
 /*---- QT Includes ----*/
 #include <QDebug>
 #include <QApplication>
-#include <QPushButton>
-#include <QWidget>
-
 
 int main(int p_argc, char* p_argv[])
 {
 	//Create the QApplication to manage the GUI
 	QApplication app(p_argc, p_argv);
 
-	//Create main widget
-	QWidget mainWidget;
-	mainWidget.setFixedSize(500, 500);
-
-	//Create a button and give the containing widget as a pointer
-	QPushButton button("First button", &mainWidget);
-
-	//Set new width and height
-	button.setFixedSize(100, 50);
-
-	// Move the button inside the main widget
-	button.move(200, 200);
-
-	//Show the main widget
-	mainWidget.show();
+	//Create our own widget and call show()
 
 	//Start application / start the events loop
 	//The GUI is not displayed if the exec() method is not called
