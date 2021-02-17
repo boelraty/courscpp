6ea174f5425d8f1bfe2434770f82adf95adb1702
vsimoes29 <vincent.simoes@imascap.com>
2021-02-17 16:51:49 +0100

    VTK Ex13

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <vtkSmartPointer.h>
 #include <vtkCubeSource.h>
 #include <vtkSphereSource.h>
+#include <vtkBooleanOperationPolyDataFilter.h>
 
 /*---- QT Includes ----*/
 #include <qdebug.h>
@@ -20,25 +21,37 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	// Create a sphere - radius = 50
-	vtkSmartPointer<vtkSphereSource> sphereObject = //...
+	// Create a cube
+	vtkSmartPointer<vtkSphereSource> sphereObject = vtkSmartPointer<vtkSphereSource>::New();
+	sphereObject->SetCenter(40, 0, 0);
+    sphereObject->SetRadius(50);
+    sphereObject->SetPhiResolution(50);
+    sphereObject->SetThetaResolution(50);
+	sphereObject->Update();
 
-	// Create a aphere  - radius = 45
-	vtkSmartPointer<vtkSphereSource> sphereObject2 = //...
+	// Create a cube
+	vtkSmartPointer<vtkSphereSource> sphereObject2 = vtkSmartPointer<vtkSphereSource>::New();
+	sphereObject2->SetCenter(0, 0, 0);
+    sphereObject2->SetRadius(45);
+    sphereObject2->SetPhiResolution(50);
+    sphereObject2->SetThetaResolution(50);
+	sphereObject2->Update();
 
-    vtkSmartPointer<vtkBooleanOperationPolyDataFilter> booleanFilter = //...
+    vtkSmartPointer<vtkBooleanOperationPolyDataFilter> booleanFilter = vtkSmartPointer<vtkBooleanOperationPolyDataFilter>::New();
+    booleanFilter->SetInputData(0, sphereObject->GetOutput());
+    booleanFilter->SetInputData(1, sphereObject2->GetOutput());
+    booleanFilter->SetOperationToDifference();//Difference, Intersection
+    booleanFilter->Update();
 
 	// Create mapper for the sphere
-	vtkSmartPointer<vtkPolyDataMapper> mapper = //...
+	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputData(booleanFilter->GetOutput());
 
 	// Create actor related to previous mapper
-	vtkSmartPointer<vtkActor> actor = //...
+	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+	actor->GetProperty()->SetColor(1, 0, 0);
 
-	// Create renderer
-	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-
-	// Set background color
-	renderer->SetBackground(1, 1, 1);
 
 	// Create renderer
 	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
@@ -56,6 +69,7 @@ int main(int p_argc, char* p_argv[])
 	interactorWindow->SetRenderWindow(renderWindow);
 
 	// Add actor to renderer
+	renderer->AddActor(actor);
 
 	// Start rendering
 	renderWindow->Render();
