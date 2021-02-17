5bebb1144eb1c5205c844d3f2fb14cca400e5724
vsimoes29 <vincent.simoes@imascap.com>
2021-02-17 17:27:01 +0100

    Qt Ex2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple1
+/*!	\brief	Exemple2
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
@@ -17,6 +17,22 @@ int main(int p_argc, char* p_argv[])
 	//Create a button
 	QPushButton button("First button");
 
+
+	//Set a new text to the button
+	button.setText("New text for button");
+
+	//Set new width and height
+	button.setFixedWidth(400);
+	button.setFixedHeight(200);
+
+	//Set a tool tip text
+	button.setToolTip("This is a button");
+
+	//Set a new font to the text
+	button.setFont(QFont("Courier", 20, QFont::Bold));
+
+	// ...
+
 	//Show the button
 	button.show();
 
@@ -25,4 +41,4 @@ int main(int p_argc, char* p_argv[])
 	app.exec();
 
 	return 0;
-} 
+}
